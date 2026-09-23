114590593045461b160dff72af6cf1d660f1d865
IvanHua2004 <ivan.hua@polymtl.ca>
2026-09-22 21:01:58 -0400

    Time: 23 ms (84.45%), Space: 49.6 MB (73.54%) - LeetHub

---
+++ 2043-simple-bank-system/2043-simple-bank-system.py
@@ -0,0 +1,28 @@
+class Bank:
+
+    def __init__(self, balance: list[int]):
+        self.balance = balance
+
+    def valid(self, account: int) -> bool:
+        return 1 <= account <= len(self.balance)
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if not self.valid(account1) or not self.valid(account2):
+            return False
+        if self.balance[account1-1] < money:
+            return False
+        self.balance[account1-1] -= money
+        self.balance[account2-1] += money
+        return True
+
+    def deposit(self, account: int, money: int) -> bool:
+        if not self.valid(account):
+            return False
+        self.balance[account-1] += money
+        return True
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if not self.valid(account) or self.balance[account-1] < money:
+            return False
+        self.balance[account-1] -= money
+        return True
