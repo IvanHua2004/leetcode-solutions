aefe3cb4bf1f82f77f0c84019ce313b3d528476d
IvanHua2004 <ivan.hua@polymtl.ca>
2026-09-25 13:32:38 -0400

    Time: 19 ms (8.81%), Space: 20.5 MB (32.18%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def repeatedNTimes(self, nums: list[int]) -> int:
+        hashmap = {}
+        for i in nums:
+            if i not in hashmap:
+                hashmap[i] = 1
+            else:
+                hashmap[i] += 1
+                if hashmap[i] == len(nums)//2:
+                    return i
